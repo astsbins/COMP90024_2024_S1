bc505625088bd0cfe150119c59275fb941519567
asts <astsbins@gmail.com>
2024-03-10 23:20:14 +1100

    Basic simple solution no MPI

--- load_data.py
+++ load_data.py
@@ -1,20 +1,86 @@
 # %% 
 import pandas as pd
 import json
+# TODO: Check for duplicate Ids
+# TODO: decide on outliers, dicts ints -3
 # %%
-
-df = pd.read_json("twitter-450mb.json")
-
-# %%
-df = pd.read_json("twitter-50mb.json")
+df = pd.read_json("twitter-1mb.json", orient = "records")
 
 # %%
-d = json.load(open('twitter-450mb.json', encoding="utf-8"))
-df = pd.DataFrame.from_dict(d, orient="index")
+from tabulate import tabulate
+sentiment0 = df.rows[0]["doc"]["data"]["sentiment"]
+time0 = df.rows[0]["doc"]["data"]["created_at"][:-11]
+sentiment0
+time0
 # %%
-df["value"]
+sentiments = []
+for i in range(len(df.rows)-1):
+    tweet_data = df.rows[i]["doc"]["data"]
+    if "sentiment" in tweet_data:
+        if type(tweet_data["sentiment"]) is  int:
+            # print(type(tweet_data["sentiment"])) 
+            # print (tabulate([tweet_data["sentiment"], tweet_data["text"]]))
+            sentiments.append([tweet_data["sentiment"], tweet_data["text"]])
+            
 # %%
-for col in df.columns:
-    print(col)
+sentiments.sort(reverse=True)
+
+print(tabulate(sentiments))        
 # %%
-df["rows"][0]["data"]
+scores = []
+time_sentimentDict = {}
+for i in range(len(df.rows)-1):
+    tweet_data = df.rows[i]["doc"]["data"]
+    try:
+        if "sentiment" in tweet_data:
+            current_tweet_time = tweet_data["created_at"][:-11]
+            current_tweet_sentiment = tweet_data["sentiment"]
+            if type(current_tweet_sentiment) is dict:
+                current_tweet_sentiment = current_tweet_sentiment["score"]
+        else:
+            continue
+    except Exception as e:
+        print("error", e)
+        print(i,df.rows[i]["doc"]["data"])
+        # print e
+        break
+    if current_tweet_time in time_sentimentDict:
+        time_sentimentDict[current_tweet_time].append(current_tweet_sentiment)
+    else:
+        time_sentimentDict[current_tweet_time] = [current_tweet_sentiment]
+        
+    scores.append([current_tweet_sentiment, tweet_data["text"]])
+        
+# %%
+maxhour = ''
+maxhourtweets = 0
+maxSentiment = ''
+maxsumsentiment = 0
+for hour in time_sentimentDict:
+    try:
+        if len(time_sentimentDict[hour]) > maxhourtweets:
+            maxhour = hour
+            maxhourtweets = len(time_sentimentDict[hour])
+        
+        if sum(time_sentimentDict[hour]) > maxsumsentiment:
+            maxSentiment = hour
+            maxsumsentiment = sum(time_sentimentDict[hour])
+            print(maxsumsentiment)
+    except Exception as e:
+        print(e)
+        print(time_sentimentDict[hour])
+        print(len(time_sentimentDict[hour]))
+        break
+
+print("max hour tweet", maxhour)
+print("max hour sentiment", maxsumsentiment)
+
+# %%
+df["rows"][0].keys()
+
+# %%
+value1 = df["rows"][0]
+value1["doc"]
+# %%
+from mpi4py import MPI
+# %%
---
+++ x
@@ -0,0 +1,13 @@
+{'score': 0, 'comparative': 0, 'calculation': [], 'tokens': ['@rociomag3', '@socorro49046006', '👏🏼👏🏼👏🏼👏🏼👏🏼😘'], 'words': [], 'positive': [], 'negative': []}
+{'score': 0, 'comparative': 0, 'calculation': [], 'tokens': ['@cyph3r', 'asr', '@asjad', 'august', '@pentesterlab', 'https', 't', 'co', 'pcm4f27bvx'], 'words': [], 'positive': [], 'negative': []}
+{'score': 0, 'comparative': 0, 'calculation': [], 'tokens': ['@joedaddy2654', 'https', 't', 'co', 'twqxm0oji1', '🤍🤍'], 'words': [], 'positive': [], 'negative': []}
+{'score': 0, 'comparative': 0, 'calculation': [], 'tokens': ['@styiesicon', 'no😐'], 'words': [], 'positive': [], 'negative': []}
+{'score': 0, 'comparative': 0, 'calculation': [], 'tokens': ['@kjr', 'sora', 'えろい！！！🤣🤣'], 'words': [], 'positive': [], 'negative': []}
+{'score': 0, 'comparative': 0, 'calculation': [], 'tokens': ['@whcos', '@presssec', '@reppeterdefazio', 'l', 'https', 't', 'co', 'cprzqwwyd7'], 'words': [], 'positive': [], 'negative': []}
+{'score': 0, 'comparative': 0, 'calculation': [], 'tokens': ['@presssec', 'l', 'https', 't', 'co', 'cprzqwwyd7'], 'words': [], 'positive': [], 'negative': []}
+{'score': 0, 'comparative': 0, 'calculation': [], 'tokens': ['@flightl3ssbird', '@emersonbarrett', '@emersonbarrett'], 'words': [], 'positive': [], 'negative': []}
+{'score': -3, 'comparative': -0.5, 'calculation': [{'illegal': -3}], 'tokens': ['@dat', 'corgi', '@aurorachipmunk', '@stormykittyhawk', 'see', 'illegal'], 'words': ['illegal'], 'positive': [], 'negative': ['illegal']}
+{'score': 0, 'comparative': 0, 'calculation': [], 'tokens': ['@victori45792596', '@ukchange', 'sst'], 'words': [], 'positive': [], 'negative': []}
+{'score': 0, 'comparative': 
+
+{'author_id': '914601022051336192', 'conversation_id': '1405853924251226112', 'created_at': '2021-06-21T11:09:17.000Z', 'entities': {'mentions': [{'start': 0, 'end': 16, 'username': 'AkiyoshiKitaoka'}]}, 'geo': {}, 'lang': 'en', 'public_metrics': {'retweet_count': 0, 'reply_count': 0, 'like_count': 0, 'quote_count': 0}, 'text': '@AkiyoshiKitaoka Can you point to some of information about this type of illusion?', 'sentiment': 0}
